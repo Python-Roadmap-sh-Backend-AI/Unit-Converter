90e5ffda9ccb4f181591907198d651021c35190f
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-06-05 20:28:29 +0530

    feat:lenght converter is done

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Unit Converter"
+    return render_template("index.html")
+
+@app.route("/length", methods=["POST","GET"])
+def length():
+    result = None
+
+    length_units = {
+        "millimeter": 0.001,
+        "centimeter": 0.01,
+        "meter": 1,
+        "kilometer": 1000,
+        "inch": 0.0254,
+        "foot": 0.3048,
+        "yard": 0.9144,
+        "mile": 1609.34
+    }
+    if request.method == "POST":
+        value = float(request.form.get("value"))
+        from_unit = request.form.get("from_unit")
+        to_unit = request.form.get("to_unit")
+
+        base_value = value * length_units[from_unit]
+        result = base_value / length_units[to_unit]
+
+        print(value, from_unit, to_unit)
+    return render_template("length.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
